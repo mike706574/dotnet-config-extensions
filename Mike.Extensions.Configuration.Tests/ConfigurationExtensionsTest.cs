d6aad5c1af5f1df6fa8098b5d5f5511ea79eee4e
EAS7510 <michaeleaster@northwesternmutual.com>
2018-07-04 10:16:29 -0500

    IConfiguration extensions.

---
+++ Mike.Extensions.Configuration.Tests/ConfigurationExtensionsTest.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Mike.Extensions.Configuration.Tests
+{
+    public class ConfigurationExtensionsTest
+    {
+        private enum Foo
+        {
+            Bar,
+            Baz
+        }
+
+        [Fact]
+        public void RequiredEnum()
+        {
+            IConfiguration config = BuildConfig(new Dictionary<string, string>
+            {
+                {"Foo", "Bar"}
+            });
+            Foo foo = config.GetRequiredEnumValue<Foo>("Foo");
+            Assert.Equal(Foo.Bar, foo);
+        }
+
+        [Fact]
+        public void MissingRequiredEnum()
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+            {
+                IConfiguration config = BuildConfig(new Dictionary<string, string>());
+                config.GetRequiredEnumValue<Foo>("Foo");
+            });
+
+            Assert.Equal("No value found for required configuration property \"Foo\".",
+                         exception.Message);
+        }
+
+        [Fact]
+        public void InvalidRequiredEnum()
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+            {
+                IConfiguration config = BuildConfig(new Dictionary<string, string>
+                {
+                    {"Foo", "Whatever"}
+                });
+
+                config.GetRequiredEnumValue<Foo>("Foo");
+            });
+
+            Assert.Equal(
+                "Invalid value \"Whatever\" for configuration property \"Foo\". Valid options: \"Bar\", \"Baz\"",
+                exception.Message);
+        }
+
+        [Fact]
+        public void RequiredValue()
+        {
+            IConfiguration config = BuildConfig(new Dictionary<string, string>()
+            {
+                {"Foo", "Bar"}
+            });
+
+            string foo = config.GetRequiredValue<string>("Foo");
+
+            Assert.Equal("Bar", foo);
+        }
+
+        [Fact]
+        public void MissingRequiredValue()
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() =>
+            {
+                IConfiguration config = BuildConfig(new Dictionary<string, string>());
+
+                config.GetRequiredValue<string>("Foo");
+            });
+
+            Assert.Equal("No value found for required configuration property \"Foo\".",
+                         exception.Message);
+        }
+
+        private IConfiguration BuildConfig(IDictionary<string, string> props)
+        {
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(props)
+                .Build();
+        }
+    }
+}
---
+++ Mike.Extensions.Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Mike.Extensions.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static T GetRequiredValue<T>(this IConfiguration config, string key)
+        {
+            T value = config.GetValue<T>(key);
+
+            if (value == null)
+            {
+                string message = $"No value found for required configuration property \"{key}\".";
+                throw new ArgumentException(message);
+            }
+
+            return value;
+        }
+
+        public static T GetRequiredEnumValue<T>(this IConfiguration config, string key)
+        {
+            string value = config.GetValue<string>(key);
+
+            if (value == null)
+            {
+                string message = $"No value found for required configuration property \"{key}\".";
+                throw new ArgumentException(message);
+            }
+
+            try
+            {
+                T type = (T) Enum.Parse(typeof(T), value);
+                return type;
+            }
+            catch (ArgumentException ex)
+            {
+                string options = string.Join(", ", ((T[]) Enum.GetValues(typeof(T)))
+                                             .Select(option => $"\"{option}\""));
+
+                string message = $"Invalid value \"{value}\" for configuration property \"{key}\". Valid options: {options}";
+                throw new ArgumentException(message, ex);
+            }
+        }
+    }
+}
